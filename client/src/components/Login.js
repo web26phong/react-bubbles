31b5b70558c599d0517a76c6a8d3c45053037b91
Phong Yang <phongyang88@hotmail.com>
2020-01-31 10:27:37 -0600

    routes to bubblepage after login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -21,8 +21,8 @@ const handleSubmit = e => {
     Axios
     .post(`http://localhost:5000/api/login`, {username, password})
     .then(res=> {
-      console.log(res)
-        // localStorage.setItem("token", res.data.payload);
+      // console.log(res)
+        localStorage.setItem("token", res.data.payload);
         props.history.push("/bubblepage")
         setMessage("")
     })
